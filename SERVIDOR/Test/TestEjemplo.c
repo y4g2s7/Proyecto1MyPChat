81cac0653ea54c31700e35d097f6b8d08b617bf2
Giovanni Santillan <giovas@ciencias.unam.mx>
2026-09-09 23:26:20 -0600

    Test de verdad para ver si el servidor separa los mensajes y ignora uno vacio

--- SERVIDOR/Test/TestEjemplo.c
+++ SERVIDOR/Test/TestEjemplo.c
@@ -1,6 +1,20 @@
 #include <assert.h>
+#include <string.h>
+#include "Servidor/SocketServidor.h"
+
+void TestMensajeVacio(){
+  char buffer[50]={0};
+  
+  char *inicioMensaje=buffer;
+  char *mensajes[20];
+  strcpy(buffer, "hola\n\nadios\n");
+  int bytesAcumulados=strlen(buffer);
+  int numeroMensajes = procesarBuffer(buffer,&bytesAcumulados,&inicioMensaje,mensajes);
+  assert(2==numeroMensajes);
+}
 
 int main() {
-    assert(1 + 1 == 2);
-    return 0;
+  TestMensajeVacio();
+  return 0;
 }
+
